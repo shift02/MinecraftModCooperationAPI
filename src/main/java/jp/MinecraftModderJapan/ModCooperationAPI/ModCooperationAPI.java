a21427d7b5d589578aeb0247c527330701cff2c3
CrafterKina <kina_ugo@yahoo.co.jp>
2016-03-12 19:18:02 +0900

    create core files

---
+++ src/main/java/jp/MinecraftModderJapan/ModCooperationAPI/ModCooperationAPI.java
@@ -0,0 +1,8 @@
+package jp.MinecraftModderJapan.ModCooperationAPI;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = ModCooperationAPI.MOD_ID)
+public class ModCooperationAPI{
+    public static final String MOD_ID = "jp.MinecraftModderJapan.ModCooperationAPI";
+}
