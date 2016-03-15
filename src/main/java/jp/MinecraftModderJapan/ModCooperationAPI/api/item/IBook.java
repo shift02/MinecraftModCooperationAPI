dd0e6e8edfebdb3c90fe2726e02feef8fbdfe04b
shift02 <shift02ss@gmail.com>
2016-03-15 15:01:21 +0900

    修正：Javadoc

--- src/main/java/jp/MinecraftModderJapan/ModCooperationAPI/api/item/IBook.java
+++ src/main/java/jp/MinecraftModderJapan/ModCooperationAPI/api/item/IBook.java
@@ -3,21 +3,26 @@ package jp.MinecraftModderJapan.ModCooperationAPI.api.item;
 import net.minecraft.item.ItemStack;
 
 /**
- * 本のためのインターフェイス
+ * 本のためのインターフェイス Interface for the book
  *
  * @author Shift02
  * @version 1.0.0
+ * @since 1.0.0
  */
 public interface IBook {
 
 	/**
-	 * GUIを持っているか
-	 * @return 持っている場合はtrue
+	 * GUIを持っているか have the book
+	 * @return 持っている場合はtrue  If the book has "true"
+	 * @version 1.0.0
+     * @since 1.0.0
 	 */
 	boolean hasGUI(ItemStack book);
 
 	/**
-	 * GUIを開く
+	 * GUIを開く Open the GUI
+	 * @version 1.0.0
+     * @since 1.0.0
 	 */
 	void openGUI(ItemStack book);
 
